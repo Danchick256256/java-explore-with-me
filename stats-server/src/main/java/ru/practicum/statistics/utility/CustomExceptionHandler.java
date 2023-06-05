da5be81d9f6b7f8939692d5549832e5839a95fda
Danchick256256 <danilfpronkin280504@gmail.com>
2023-06-06 01:09:48 +0300

    new chagnes

--- stats-server/src/main/java/ru/practicum/statistics/utility/CustomExceptionHandler.java
+++ stats-server/src/main/java/ru/practicum/statistics/utility/CustomExceptionHandler.java
@@ -2,18 +2,11 @@ package ru.practicum.statistics.utility;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 
-import javax.validation.ConstraintViolationException;
 import javax.xml.bind.ValidationException;
 import java.time.LocalDateTime;
 
